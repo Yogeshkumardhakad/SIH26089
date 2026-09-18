4e9ae7bd8afe49959a10ad540611dedabe680948
Yogeshkumardhakad <yogeshd.ug24.ec@nitp.ac.in>
2026-09-18 21:07:54 +0530

    error

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'uxytdhgg',
-    api_key: '885774474371139',
-    api_secret: '39Owzd1XVoTEgNNwsJ385KnyDXU'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
